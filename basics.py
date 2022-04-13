ea53133b9f279f072e49d613ba52562c015442c1
Subhash Kumar <85139394+subhash1e@users.noreply.github.com>
2022-04-13 11:11:46 -0700

    Create basics.py

---
+++ basics.py
@@ -0,0 +1,34 @@
+
+
+print("9"*5)
+#convert string to int,float
+x = "54321"
+X = int(x)
+Y = float(x)
+print(x,X,Y)
+print("cube of 9= " , 9**3)
+x = '''
+abcd
+    '''
+print(x)
+x = "abcd"
+#negative index
+print(x[-1])
+#array slicing
+print(x[1:3],x[1:],x[:1],x[:],x[:-1])
+#string concat
+#f for formatted string
+y = f'{x} is string\t'
+print(y, x+" String")
+#s.length  = len(s) // this is gpp function
+#s.upper() // this is method of string class
+#s.find('P') // finds index of char P -1 if not found
+#s.replace('hello', 'hi') // replace hello to hi in string s
+#bool x = 'Py' in 'Python' // in operator checks and produce Boolean value
+print('Py' in 'Python')
+
+#gpp function for numbers are
+#after importing math 
+#round() abs() ceil() floor() use as math.round()
+#condition operator are : and , or , and not, not
+#comparision operator are : == , > , < , >= , <=
